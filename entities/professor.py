b77d5261ae082f19372d456f8d8f1ab6cdd1dc75
nooyag8 <qkrrkdbs814@gmail.com>
2025-11-29 18:49:51 +0900

    fix: professor 상태에 따른 행동 수정 및 이미지 위치 수정

--- entities/professor.py
+++ entities/professor.py
@@ -21,7 +21,10 @@ class Professor:
         # 이미지 로드
         self.image_writing = pygame.image.load("OOP-final-project/images/professorBack.png")
         self.image_watching = pygame.image.load("OOP-final-project/images/professorFront.png")
-        self.current_image = self.image_writing
+        self.current_image = self.image_writing #현재 이미지
+
+        self.rect = self.current_image.get_rect()
+        self.rect.center = (self.x, self.y)
 
     def update(self, dt):
         # 감시 중일 때 (Watching)
@@ -44,15 +47,24 @@ class Professor:
         self.state = "Watching"
         self.watch_timer = duration
         self.current_image = self.image_watching
+
+        #이미지 변경시 rect도 해당 이미지 기준으로 다시 만들어 같은 위치 유지
+        old_center = self.rect.center
+        self.rect = self.current_image.get_rect()
+        self.rect.center = old_center
+
         print(f"(!) 교수님이 뒤를 돌아봅니다! ({duration:.1f}초간 감시)")
 
     def professor_stop(self):
         self.state = "Writing"
         self.write_timer = random.uniform(self.min_write, self.max_write)
         self.current_image = self.image_writing
+
+        old_center = self.rect.center
+        self.rect = self.current_image.get_rect()
+        self.rect.center = old_center
+        
         print("교수님이 다시 수업을 시작합니다!")
 
     def draw(self, surface: pygame.Surface):
-        rect = self.init_image.get_rect()
-        rect.center = (self.x, self.y)
-        surface.blit(self.init_image, rect)
+        surface.blit(self.current_image, self.rect)
